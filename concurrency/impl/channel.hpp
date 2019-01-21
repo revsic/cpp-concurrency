de51fac6c8715c761b8306b9ac3708cc3b00e431
revsic <revsic99@gmail.com>
2019-01-22 01:09:59 +0900

    Update Channel - Remove RingBuffer impl

--- concurrency/impl/channel.hpp
+++ concurrency/impl/channel.hpp
@@ -1,70 +1,13 @@
 #ifndef CHANNEL_HPP
 #define CHANNEL_HPP
 
+#include "ring_buffer.hpp"
+
 #include <condition_variable>
 #include <list>
 #include <mutex>
 #include <optional>
 #include <thread>
-#include <type_traits>
-
-template <typename T,
-          typename = std::enable_if_t<std::is_default_constructible_v<T>>>
-class RingBuffer {
-public:
-    RingBuffer() : RingBuffer(1) {
-        // Do Nothing
-    }
-
-    RingBuffer(size_t size_buffer) :
-        size_buffer(size_buffer), buffer(std::make_unique<T[]>(size_buffer))
-    {
-        // Do Nothing
-    }
-
-    RingBuffer(const RingBuffer&) = delete;
-    RingBuffer(RingBuffer&&) = delete;
-
-    RingBuffer& operator=(const RingBuffer&) = delete;
-    RingBuffer& operator=(RingBuffer&&) = delete;
-
-    template <typename... U>
-    void emplace_back(U&&... args) {
-        buffer[ptr_tail] = T(std::forward<U>(args)...);
-
-        num_data += 1;
-        ptr_tail = (ptr_tail + 1) % size_buffer;
-    }
-
-    void pop_front() {
-        num_data -= 1;
-        ptr_head = (ptr_head + 1) % size_buffer;
-    }
-
-    T& front() {
-        return buffer[ptr_head];
-    }
-
-    const T& front() const {
-        return buffer[ptr_head];
-    }
-
-    size_t size() const {
-        return num_data;
-    }
-
-    size_t max_size() const {
-        return size_buffer;
-    }
-
-private:
-    size_t size_buffer;
-    std::unique_ptr<T[]> buffer;
-
-    size_t num_data = 0;
-    size_t ptr_head = 0;
-    size_t ptr_tail = 0;
-};
 
 template <typename T,
           typename Container = RingBuffer<T>> // or Container = std::list<T>
