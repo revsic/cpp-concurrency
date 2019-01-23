ce60d3195e82db869d3d961ad8f44a8c29cbf542
revsic <revsic99@gmail.com>
2019-01-24 02:06:59 +0900

    Update Platform - Implement optional for macosx

---
+++ concurrency/impl/platform/optional.hpp
@@ -0,0 +1,38 @@
+#ifndef PLATFORM_OPTIONAL_HPP
+#define PLATFORM_OPTIONAL_HPP
+
+#ifndef __APPLE__
+#include <optional>
+#else
+#include <experimental/optional>
+#endif
+
+namespace platform {
+#ifndef __APPLE__
+    constexpr auto nullopt = std::nullopt;
+
+    template <typename T>
+    using optional = std::optional<T>;
+
+#else
+    constexpr auto nullopt = std::experimental::nullopt;
+
+    template <typename T>
+    class optional : public std::experimental::optional<T> {
+    public:
+        constexpr T& value() & {
+            return **this;
+        }
+
+        constexpr T&& value() && {
+            return std::move(**this);
+        }
+
+        constexpr bool has_value() const noexcept {
+            return static_cast<bool>(*this);
+        }
+    };
+#endif
+}
+
+#endif
