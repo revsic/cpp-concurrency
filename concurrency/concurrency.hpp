32f89f9764a2053fe40545345a15142a317f83c0
revsic <revsic99@gmail.com>
2019-01-24 04:17:04 +0900

    Update Concurrency - Reorder import

--- concurrency/concurrency.hpp
+++ concurrency/concurrency.hpp
@@ -1,8 +1,8 @@
 #ifndef CONCURRENCY_HPP
 #define CONCURRENCY_HPP
 
-#include "impl/ring_buffer.hpp"
 #include "impl/channel.hpp"
+#include "impl/ring_buffer.hpp"
 #include "impl/thread_pool.hpp"
 #include "impl/wait_group.hpp"
 
