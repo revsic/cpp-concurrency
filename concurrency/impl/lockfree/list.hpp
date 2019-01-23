574a2cbf0a915b1f52b3f32978ab89c55dbab4cd
revsic <revsic99@gmail.com>
2019-01-24 02:24:46 +0900

    Update Lockfree/List - Fix template issue

--- concurrency/impl/lockfree/list.hpp
+++ concurrency/impl/lockfree/list.hpp
@@ -77,7 +77,7 @@ namespace LockFree {
                     T res = std::move(node->data);
                     delete node;
 
-                    return platform::optional(std::move(res));
+                    return platform::optional<T>(std::move(res));
                 }
             }
             return platform::nullopt;
