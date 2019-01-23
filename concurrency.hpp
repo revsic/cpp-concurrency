14d7e860e6ca198d9cd3665a71ed5b5fc6984a01
revsic <revsic99@gmail.com>
2019-01-24 02:19:45 +0900

    Update Concurrency - Remerge

--- concurrency.hpp
+++ concurrency.hpp
@@ -20,63 +20,38 @@
 
 
 #ifndef __APPLE__
-
 #include <optional>
-
 #else
-
 #include <experimental/optional>
-
 #endif
 
 
 namespace platform {
-
 #ifndef __APPLE__
-
     constexpr auto nullopt = std::nullopt;
 
-
     template <typename T>
-
     using optional = std::optional<T>;
 
-
 #else
-
     constexpr auto nullopt = std::experimental::nullopt;
 
-
     template <typename T>
-
     class optional : public std::experimental::optional<T> {
-
     public:
-
         constexpr T& value() & {
-
             return **this;
-
         }
-
 
         constexpr T&& value() && {
-
             return std::move(**this);
-
         }
-
 
         constexpr bool has_value() const noexcept {
-
             return static_cast<bool>(*this);
-
         }
-
     };
-
 #endif
-
 }
 
 
