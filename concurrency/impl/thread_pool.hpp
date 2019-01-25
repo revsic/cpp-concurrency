ac089c5b30f5bc132fa8884c288afabb738ff197
revsic <revsic99@gmail.com>
2019-01-26 04:30:29 +0900

    Update ThreadPool - Add LockFree ThreadPool

--- concurrency/impl/thread_pool.hpp
+++ concurrency/impl/thread_pool.hpp
@@ -4,10 +4,15 @@
 #include <future>
 
 #include "channel.hpp"
+#include "lockfree/channel.hpp"
 
-template <typename T,
-          template <typename Elem, typename = std::allocator<Elem>>
-          class Container = RingBuffer>
+template <typename T, template <typename> class ChannelType>
+using ChannelTypeGen = ChannelType<std::packaged_task<T()>>;
+
+template <typename T, template <typename, template <typename, typename> class> class ChannelType,  template <typename, typename> class Container>
+using ChannelTypeGen2 = ChannelType<std::packaged_task<T()>, Container>;
+
+template <typename T, typename ChannelType = ChannelTypeGen2<T, Channel, RingBuffer>>
 class ThreadPool {
 public:
     ThreadPool() : ThreadPool(std::thread::hardware_concurrency()) {
@@ -16,9 +21,10 @@ public:
 
     template <typename... Args>
     ThreadPool(size_t num_threads, Args&&... args)
-        : num_threads(num_threads),
-          threads(std::make_unique<std::thread[]>(num_threads)),
-          channel(std::forward<Args>(args)...) {
+        : runnable(true),
+          num_threads(num_threads),
+          channel(std::forward<Args>(args)...),
+          threads(std::make_unique<std::thread[]>(num_threads)) {
         for (size_t i = 0; i < num_threads; ++i) {
             threads[i] = std::thread([this] {
                 while (runnable) {
@@ -69,13 +75,17 @@ public:
     }
 
 private:
-    bool runnable = true;
+    bool runnable;
     size_t num_threads;
+
+    ChannelType channel;
     std::unique_ptr<std::thread[]> threads;
-    Channel<std::packaged_task<T()>, Container> channel;
 };
 
 template <typename T>
-using UThreadPool = ThreadPool<T, std::list>;
+using UThreadPool = ThreadPool<T, ChannelTypeGen2<T, Channel, std::list>>;
+
+template <typename T>
+using LThreadPool = ThreadPool<T, ChannelTypeGen<T, LockFree::Channel>>;
 
 #endif
