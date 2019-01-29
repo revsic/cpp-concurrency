0837f02b029e647ea75ecf6f8cb03b87211791f3
revsic <revsic99@gmail.com>
2019-01-29 18:00:32 +0900

    Update Concurrency - Add dependencies

--- concurrency/concurrency.hpp
+++ concurrency/concurrency.hpp
@@ -1,9 +1,15 @@
 #ifndef CONCURRENCY_HPP
 #define CONCURRENCY_HPP
 
+#include "impl/platform/constant.hpp"
+#include "impl/platform/optional.hpp"
+#include "impl/channel_iter.hpp"
 #include "impl/channel.hpp"
 #include "impl/ring_buffer.hpp"
+#include "impl/select.hpp"
 #include "impl/thread_pool.hpp"
 #include "impl/wait_group.hpp"
+#include "impl/lockfree/channel.hpp"
+#include "impl/lockfree/list.hpp"
 
 #endif
